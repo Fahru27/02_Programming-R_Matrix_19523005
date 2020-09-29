ed3b7f5728f55daf87776852ea302cadf1d14cad
Fahru27 <nafachru@gmail.com>
2020-09-29 19:53:19 +0700

    project pertama

---
+++ 02_Programming R_Matrix_19523005.R
@@ -0,0 +1,49 @@
+#exercise 1
+a <- matrix(data = c(1:4), 2, 2, TRUE)
+a
+
+#exercise 2
+A <- matrix(data = c(1:9), 3, 3, TRUE)
+A
+A[-1,] #seluruh elemen pada baris ke-1 hilang atau dikurangi
+A[,-2] #seluruh elemen pada kolom ke-2 hilang atau dikurangi
+
+#exercise 3
+N <- matrix(data = c(1:9), 3, 3, FALSE)
+N[N > 3] <- 2
+N #semua elemen yang lebih besar dari 3 diinput ulang atau diubah menjadi angka 2
+
+#final exercise
+#1-3
+G <- matrix(data = c(1:100), 10, 10, TRUE)
+G
+
+#4.1
+H <- t(G)
+H
+
+#4.2
+J <- G+H
+J
+
+#4.3
+det(G)
+det(H)
+det(J)
+
+#4.4
+K <- cbind(G[,(1:5)], J[,(1:5)])
+K
+
+#4.5
+G
+G * solve(G)
+
+R <- G * solve(G)
+
+solve(G)
+
+D <- solve(G)
+
+#selalu error saat mencari invers G
+
